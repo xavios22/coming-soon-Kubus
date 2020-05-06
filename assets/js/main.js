88e8d60cd9cdbeee80694bea8197cfabebb0cf79
Xavier <xavier@MacBook-Air-de-Xavier.local>
2020-05-06 20:56:19 +0200

    first comment

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function () {
+
+    "use strict";
+
+    /*
+     ----------------------------------------------------------------------
+     Preloader
+     ----------------------------------------------------------------------
+     */
+    $(".loader").delay(100).fadeOut();
+    $(".animationload").delay(100).fadeOut("fast");
+    /*
+
+ ----------------------------------------------------------------------
+ Watch
+ ----------------------------------------------------------------------
+ */
+    if ($.find('#watch')[0]) {
+
+        $('#watch').countDown({
+            targetDate: {
+                'day': 1,
+                'month': 6,
+                'year': 2020,
+                'hour': 9,
+                'min': 0,
+                'sec': 0
+            },
+            omitWeeks: true
+        });
+        //enter the count down date using the format year, month, day, time: hour, min, sec
+
+        if ($('.day_field .top').html() == "0") $('.day_field').css('display', 'none');
+    }
+
+});
